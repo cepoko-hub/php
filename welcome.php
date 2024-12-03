4cb7ef32f513b8f11075ca1c4d8eadeb00617cc9
Marlay Derbecque <derbecquem@gmail.com>
2024-12-03 16:40:09 +0100

    v2

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 style="text-align: center; margin-top: 20px;">Bienvenue sur la page d'administrateur !</h1>
+</body>
+</html>
